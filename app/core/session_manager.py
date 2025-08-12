9e5a597a37ef3c34d5301b0ff36b002549b7c64a
krishvatech <info@krishvatech.com>
2025-08-12 20:55:57 +0530

    12 August Ravi - Removed Redis

--- app/core/session_manager.py
+++ app/core/session_manager.py
@@ -1,20 +1,23 @@
 # import redis.asyncio as aioredis
-# import os
-# import json
+import os
+import json
 
 # REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 # redis = aioredis.from_url(REDIS_URL, decode_responses=True)
 
-# class SessionManager:
-#     @staticmethod
-#     async def get_session(user_id: str):
-#         data = await redis.get(user_id)
-#         return json.loads(data) if data else {}
+class SessionManager:
+    @staticmethod
+    async def get_session(user_id: str):
+        # data = await redis.get(user_id)
+        # return json.loads(data) if data else {}
+        pass
 
-#     @staticmethod
-#     async def set_session(user_id: str, data: dict):
-#         await redis.set(user_id, json.dumps(data), ex=3600)
+    @staticmethod
+    async def set_session(user_id: str, data: dict):
+        # await redis.set(user_id, json.dumps(data), ex=3600)
+        pass
 
-#     @staticmethod
-#     async def clear_session(user_id: str):
-#         await redis.delete(user_id)
+    @staticmethod
+    async def clear_session(user_id: str):
+        # await redis.delete(user_id)
+        pass
