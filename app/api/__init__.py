e8ffb7a97cc2d0133daf9913023f1a14d131b467
krishvatech <info@krishvatech.com>
2025-08-13 10:05:25 +0530

    pooja 13-august whatsapp_changes

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from . import whatsapp, voice, admin
+import app.utils.whatsapp_utils as whatsapp_utils
 from .exotel_webhook import router as stream_server_router
 
 api_router = APIRouter()
 api_router.include_router(stream_server_router,prefix="/voice")
-api_router.include_router(whatsapp.router, prefix="/whatsapp", tags=["WhatsApp"])
+api_router.include_router(whatsapp_utils.router, prefix="/whatsapp", tags=["WhatsApp"]) 
 api_router.include_router(voice.router, prefix="/voice", tags=["Voice"])
 api_router.include_router(admin.router, prefix="/admin", tags=["Admin"])
